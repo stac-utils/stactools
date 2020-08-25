224d935636c8eb1ebc2106076590f03fcb16b809
Rob Emanuele <rdemanuele@gmail.com>
2020-08-25 11:17:52 -0400

    Fix fsspec STAC_IO usage.

---
+++ tests/test_io.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+from tempfile import TemporaryDirectory
+
+import pystac
+
+
+class IOTest(unittest.TestCase):
+    def test_fsspec_io(self):
+        with TemporaryDirectory() as tmp_dir:
+            catalog_url = (
+                'https://raw.githubusercontent.com/stac-utils/pystac/v0.5.2/tests/data-files/'
+                'catalogs/test-case-1/catalog.json')
+
+            cat = pystac.read_file(catalog_url)
+            col = cat.get_child('country-1')
+            self.assertEqual(len(list(col.get_children())), 2)
+
+            cat.normalize_and_save(tmp_dir, pystac.CatalogType.SELF_CONTAINED)
+
+            cat2 = pystac.read_file(os.path.join(tmp_dir, 'catalog.json'))
+            col2 = cat2.get_child('country-1')
+            self.assertEqual(len(list(col2.get_children())), 2)
