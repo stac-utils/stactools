45f1c796d8e776d17f3bd73cf56b3e4d13595f57
Rob Emanuele <rdemanuele@gmail.com>
2021-03-25 23:11:12 -0400

    Update eo:band data for aster.

--- stactools_aster/stactools/aster/constants.py
+++ stactools_aster/stactools/aster/constants.py
@@ -8,73 +8,101 @@ ASTER_FILE_NAME_REGEX = (r'AST_L1T_(?P<start>[\d]+)_'
 
 ASTER_BANDS = [
     Band.create(
-        name="B01",
-        description="VNIR_Band1 (visible green/yellow)",
+        name="VNIR_Band1",
+        common_name="yellow/green",
+        description="visible yellow/",
         center_wavelength=0.56,
+        full_width_half_max=0.08
     ),
     Band.create(
-        name="B02",
-        description="VNIR_Band2 (visible red)",
+        name="VNIR_Band2",
+        common_name="red",
+        description="visible red",
         center_wavelength=0.66,
+        full_width_half_max=0.06
     ),
     Band.create(
-        name="B3N",
-        description="VNIR_Band3N (near infrared, nadir pointing)",
+        name="VNIR_Band3N",
+        common_name="near infrared",
+        description="near infrared",
         center_wavelength=0.82,
+        full_width_half_max=0.08
+
     ),
     Band.create(
-        name="B04",
-        description="SWIR_Band4 (short-wave infrared)",
+        name="SWIR_Band4",
+        common_name="swir",
+        description="short-wave infrared",
         center_wavelength=1.65,
+        full_width_half_max=0.100
     ),
     Band.create(
-        name="B05",
-        description="SWIR_Band5 (short-wave infrared)",
+        name="SWIR_Band5",
+        common_name="swir",
+        description="short-wave infrared",
         center_wavelength=2.165,
+        full_width_half_max=0.040
     ),
     Band.create(
-        name="B06",
-        description="SWIR_Band6 (short-wave infrared)",
+        name="SWIR_Band6",
+        common_name="swir",
+        description="short-wave infrared",
         center_wavelength=2.205,
+        full_width_half_max=0.040
     ),
     Band.create(
-        name="B07",
-        description="SWIR_Band7 (short-wave infrared)",
-        center_wavelength=2.26,
+        name="SWIR_Band7",
+        common_name="swir",
+        description="short-wave infrared",
+        center_wavelength=2.260,
+        full_width_half_max=0.050
     ),
     Band.create(
-        name="B08",
-        description="SWIR_Band8 (short-wave infrared)",
-        center_wavelength=2.33,
+        name="SWIR_Band8",
+        common_name="swir",
+        description="short-wave infrared",
+        center_wavelength=2.339,
+        full_width_half_max=0.070
     ),
     Band.create(
-        name="B09",
-        description="SWIR_Band9 (short-wave infrared)",
+        name="SWIR_Band9",
+        common_name="swir",
+        description="short-wave infrared",
         center_wavelength=2.395,
+        full_width_half_max=0.070
     ),
     Band.create(
-        name="B10",
-        description="TIR_Band10 (thermal infrared)",
-        center_wavelength=8.3,
+        name="TIR_Band10",
+        common_name="lwir",
+        description="thermal infrared",
+        center_wavelength=8.300,
+        full_width_half_max=0.350
     ),
     Band.create(
-        name="B11",
-        description="TIR_Band11 (thermal infrared)",
-        center_wavelength=8.65,
+        name="TIR_Band11",
+        description="thermal infrared",
+        center_wavelength=8.650,
+        full_width_half_max=0.350
     ),
     Band.create(
-        name="B12",
-        description="TIR_Band12 (thermal infrared)",
-        center_wavelength=9.11,
+        name="TIR_Band12",
+        common_name="lwir",
+        description="thermal infrared",
+        center_wavelength=9.110,
+        full_width_half_max=0.350
     ),
     Band.create(
-        name="B13",
-        description="TIR_Band13 (thermal infrared)",
-        center_wavelength=10.6,
+        name="TIR_Band13",
+        common_name="lwir",
+        description="thermal infrared",
+        center_wavelength=10.600,
+        full_width_half_max=0.700
     ),
     Band.create(
-        name="B14",
-        description="TIR_Band14 (thermal infrared)",
-        center_wavelength=11.3,
+        name="TIR_Band14",
+        common_name="lwir",
+        description="thermal infrared",
+        center_wavelength=11.300,
+        full_width_half_max=0.700
     )
 ]
