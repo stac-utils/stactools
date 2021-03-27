68ae833be0b388fb36774514e68e5200fa99a80e
Rob Emanuele <rdemanuele@gmail.com>
2021-03-27 00:35:43 -0400

    Don't use cogification for now
    Currently the cogification would attempt to refactor all assets, even
    metadata assets. As this functionality needs some work and testing,
    remove the option from the command for now.

--- stactools_sentinel2/stactools/sentinel2/commands.py
+++ stactools_sentinel2/stactools/sentinel2/commands.py
@@ -4,7 +4,6 @@ import json
 import os
 
 from stactools.sentinel2.stac import create_item
-from stactools.sentinel2.cog import create_cogs
 
 logger = logging.getLogger(__name__)
 
@@ -24,11 +23,7 @@ def create_sentinel2_command(cli):
         '-p',
         '--providers',
         help='Path to JSON file containing array of additional providers')
-    @click.option('-c',
-                  '--cogify',
-                  is_flag=True,
-                  help='Convert each non-COG asset into a COG')
-    def create_item_command(src, dst, providers, cogify):
+    def create_item_command(src, dst, providers):
         """Creates a STAC Item for a given Sentinel 2 granule
 
         SRC is the path to the granule
@@ -43,8 +38,6 @@ def create_sentinel2_command(cli):
 
         item = create_item(src, additional_providers=additional_providers)
 
-        if cogify:
-            create_cogs(item)
         item_path = os.path.join(dst, '{}.json'.format(item.id))
         item.set_self_href(item_path)
 
