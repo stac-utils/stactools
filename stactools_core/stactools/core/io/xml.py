f494596f89054e1356410502f3f8ee48f3ca8329
Rob Emanuele <rdemanuele@gmail.com>
2021-03-27 17:44:15 -0400

    Cache XML reads; add methods for throwing if not found

--- stactools_core/stactools/core/io/xml.py
+++ stactools_core/stactools/core/io/xml.py
@@ -1,10 +1,10 @@
-from typing import List, Optional
+from functools import lru_cache
+from typing import Callable, List, Optional
 
-from pystac import STAC_IO
 from lxml import etree
 from lxml.etree import _Element as lxmlElement
 
-from stactools.core.io import ReadHrefModifier
+from stactools.core.io import ReadHrefModifier, read_text
 
 
 class XmlElement:
@@ -14,20 +14,39 @@ class XmlElement:
     def __init__(self, element: lxmlElement):
         self.element = element
 
+    @lru_cache(maxsize=100)
     def find(self, xpath: str) -> Optional["XmlElement"]:
         node = self.element.find(xpath, self.element.nsmap)
         return None if node is None else XmlElement(node)
 
+    def find_or_throw(
+            self, xpath: str,
+            get_exception: Callable[[str], Exception]) -> "XmlElement":
+        result = self.find(xpath)
+        if result is None:
+            raise get_exception(xpath)
+        return result
+
+    @lru_cache(maxsize=100)
     def findall(self, xpath: str) -> List["XmlElement"]:
         return [
             XmlElement(e)
             for e in self.element.findall(xpath, self.element.nsmap)
         ]
 
+    @lru_cache(maxsize=100)
     def find_text(self, xpath: str) -> Optional[str]:
         node = self.find(xpath)
         return None if node is None else node.text
 
+    def find_text_or_throw(self, xpath: str,
+                           get_exception: Callable[[str], Exception]) -> str:
+        result = self.find_text(xpath)
+        if result is None:
+            raise get_exception(xpath)
+        return result
+
+    @lru_cache(maxsize=100)
     def find_attr(self, attr: str, xpath: str) -> Optional[str]:
         node = self.find(xpath)
         return None if node is None else node.get_attr(attr)
@@ -36,6 +55,7 @@ class XmlElement:
     def text(self):
         return self.element.text
 
+    @lru_cache(maxsize=100)
     def get_attr(self, attr: str) -> Optional[str]:
         return self.element.get(attr, None)
 
@@ -44,8 +64,5 @@ class XmlElement:
                   href: str,
                   read_href_modifier: Optional[ReadHrefModifier] = None
                   ) -> "XmlElement":
-        if read_href_modifier is None:
-            text = STAC_IO.read_text(href)
-        else:
-            text = STAC_IO.read_text(read_href_modifier(href))
+        text = read_text(href, read_href_modifier)
         return cls(etree.fromstring(bytes(text, encoding='utf-8')))
