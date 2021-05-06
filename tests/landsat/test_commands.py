7956b4d57eac3171a03ee28279c33129cddce9a9
Rodrigo de Azevedo Carvalho <carioca_rs@yahoo.com.br>
2021-05-06 23:29:01 +1000

    Use any asset to retrieve projection information for Landsat items (#100)
    - Adds projection information from the first geotiff asset available
    - Remove .TIF from asset names
    - Added test to ensure shape, transform and csr are correctly retrieved from the geotiff image, otherwise raise an exception.
    - Moved RasterioIOError exception to inside the loop, so it would cover just rasterio clause.
    - Fixed stac_api_to_stac function, which was supposed to return an Item and was described to return a dict.
    - Created test test_transform_static_stac_asset_not_available to test when assets are not available.
    - Replaced RasterioIOError for STACError
    - Moved item.ext.enable to the top

--- tests/landsat/test_commands.py
+++ tests/landsat/test_commands.py
@@ -8,15 +8,16 @@ from stactools.landsat.commands import create_landsat_command
 from stactools.landsat.utils import (_parse_date, stac_api_to_stac,
                                      transform_mtl_to_stac,
                                      transform_stac_to_stac)
+
 from tests.utils import CliTestCase, TestData
 
 TEST_FOLDER = Path(TestData.get_path("data-files/landsat"))
 ALL_EXAMPLES = [
     "LC08_L2SR_081119_20200101_20200823_02_T2_SR_stac.json",
     "LC08_L2SP_030034_20201111_20201212_02_T1_SR_stac.json",
+    "LC08_L2SR_232122_20191218_20201023_02_T2_SR_stac.json",
     "LE07_L2SP_167064_20070321_20200913_02_T1_SR_stac.json",
     "LT05_L2SP_201034_19860504_20200917_02_T1_SR_stac.json",
-    "LC08_L2SR_232122_20191218_20201023_02_T2_SR_stac.json",
 ]
 
 
@@ -48,6 +49,34 @@ class LandsatTest(CliTestCase):
         """Load a range of STAC 0.7.0 files and convert them to STAC 1.0.0.beta.2 items"""
         for stac_file in self.landsat_stac_files:
             item = transform_stac_to_stac(pystac.Item.from_file(stac_file))
+            item.validate()
+
+    def test_transform_static_stac_asset_not_available(self):
+        """Raises an exception when geotiff asset isn't available"""
+
+        for stac_file in self.landsat_stac_files:
+            item = pystac.Item.from_file(stac_file)
+            for asset in item.assets.values():
+                # Replace a valid local file for an invalid url forcing an exception
+                asset.href = asset.href.replace(
+                    'LC08_L2SR_081119_20200101_20200823_02_T2_SR_B2_small.TIF',
+                    "just/an/invalid/url.json")
+            with self.assertRaises(pystac.STACError):
+                transform_stac_to_stac(item)
+
+    def test_transform_static_stac_missing_asset_b2_b10(self):
+        """It has to be able to gather the right information from any geotiff files"""
+
+        for stac_file in self.landsat_stac_files:
+            item = pystac.Item.from_file(stac_file)
+
+            if item.assets.get("SR_B2.TIF"):
+                item.assets.pop("SR_B2.TIF")
+
+            if item.assets.get("ST_B10.TIF"):
+                item.assets.pop("ST_B10.TIF")
+
+            item = transform_stac_to_stac(item)
             item.validate()
 
     def test_transform_dynamic_stac(self):
