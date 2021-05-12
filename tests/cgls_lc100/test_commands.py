852d08f391e9e2940d8f9b2dcfec9fec7d26536f
Pete Gadomski <pete.gadomski@gmail.com>
2021-05-12 07:30:24 -0600

    Add cgls_lc100 cog check
    This fails due to a bad `cogify` call in cgls_lc100. Also, adds a
    subsetted local file to make sure we don't run the tests forever when we
    do fix `cogify`.

--- tests/cgls_lc100/test_commands.py
+++ tests/cgls_lc100/test_commands.py
@@ -6,7 +6,13 @@ import pystac
 from stactools.cgls_lc100.commands import create_cgls_lc100_command
 from tests.utils import (TestData, CliTestCase)
 
-EXTERNAL_DATA_PATH = 'cgls_lc100/PROBAV_LC100_global_v3.0.1_2019-nrt_ccl.tif'
+# Created from:
+#
+#   https://zenodo.org/record/3939050/files/
+#   PROBAV_LC100_global_v3.0.1_2019-nrt_Change-Confidence-layer_EPSG-4326.tif?download=1
+#
+# and subsetted via `gdal_translate -projwin -180 80 -179 79`
+DATA_PATH = 'data-files/cgls_lc100/PROBAV_LC100_global_v3.0.1_2019-nrt_ccl_subset.tif'
 
 
 class CreateItemTest(CliTestCase):
@@ -14,7 +20,7 @@ class CreateItemTest(CliTestCase):
         return [create_cgls_lc100_command]
 
     def test_create_item(self):
-        tif_href = TestData.get_external_data(EXTERNAL_DATA_PATH)
+        tif_href = TestData.get_path(DATA_PATH)
 
         with TemporaryDirectory() as tmp_dir:
             cmd = ['cgls_lc100', 'create-item', '--cogify', tif_href, tmp_dir]
@@ -26,5 +32,8 @@ class CreateItemTest(CliTestCase):
             item_path = os.path.join(tmp_dir, jsons[0])
 
             item = pystac.read_file(item_path)
+            cog = item.assets["cog_image"]
+            self.assertTrue(os.path.exists(cog.href),
+                            f"COG does not exist: {cog.href}")
 
         item.validate()
