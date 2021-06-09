8eaa943ead941445c456b16e8567996283af15ca
volaya <volayaf@gmail.com>
2021-06-09 08:54:08 +0200

    added version command
    fixes #123

---
+++ stactools_cli/stactools/cli/commands/version.py
@@ -0,0 +1,16 @@
+from click import echo
+from pystac.version import get_stac_version
+from stactools.core.version import __version__
+
+
+def create_version_command(cli):
+    @cli.command(
+        'version',
+        short_help='Display version info.')
+    def version_command():
+        """Display version info
+        """
+        echo(f"stactools version {__version__}")
+        echo(f"PySTAC version {get_stac_version()}")
+
+    return version_command
---
+++ tests/cli/commands/test_version.py
@@ -0,0 +1,18 @@
+from pystac.version import get_stac_version
+from stactools.core.version import __version__
+from stactools.cli.commands.version import create_version_command
+
+from tests.utils import (CliTestCase)
+
+
+class VersionTest(CliTestCase):
+
+    def create_subcommand_functions(self):
+        return [create_version_command]
+
+    def test_hello_world(self):
+        result = self.run_command(['version'])
+        self.assertEqual(0, result.exit_code)
+        expected = (f"stactools version {__version__}\n"
+                    f"PySTAC version {get_stac_version()}\n")
+        self.assertEqual(expected, result.output)
