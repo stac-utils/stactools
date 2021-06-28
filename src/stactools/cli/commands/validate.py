07f092f98655a341bfef2aac87990e5090ab72b0
Pete Gadomski <pete.gadomski@gmail.com>
2021-06-28 11:33:58 -0600

    Add intelligent missing link checking to validate

--- src/stactools/cli/commands/validate.py
+++ src/stactools/cli/commands/validate.py
@@ -23,9 +23,26 @@ def create_validate_command(cli):
             else:
                 object.validate_all()
             print(f"OK! STAC object at {href} is valid!")
+        except FileNotFoundError as e:
+            print(
+                f"FileNotFound error: {e}\nWalking children to find location of missing link(s)..."
+            )
+            find_missing_links(object, object)
         except pystac.STACValidationError as e:
             print(e)
             print(e.source)
             sys.exit(1)
 
     return validate_command
+
+
+def find_missing_links(root, object):
+    for child in object.get_children():
+        try:
+            find_missing_links(root, child)
+        except FileNotFoundError:
+            for link in child.get_child_links():
+                try:
+                    link.resolve_stac_object(root)
+                except FileNotFoundError:
+                    print(f"{child.self_href} has a missing link: {link.href}")
