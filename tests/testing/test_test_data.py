da35a91052a7eaa9d2742c6415d6f0281b5c0c1e
Pete Gadomski <pete.gadomski@gmail.com>
2021-07-23 12:23:04 -0700

    Add s3 configuration to testing's external data

--- tests/testing/test_test_data.py
+++ tests/testing/test_test_data.py
@@ -13,16 +13,29 @@ class TestDataTest(TestCase):
         self.assertEqual(item.id, "20201211_223832_CS2")
 
     def test_external_data_s3(self):
-        try:
-            import s3fs  # noqa
-        except ImportError:
-            raise SkipTest(
-                "Skipping testing external data with s3 because s3fs is not installed"
-            )
+        skip_without_s3fs()
         path = test_data.get_external_data("goes-16/index.html")
         with open(path) as f:
             html = f.read()
         self.assertNotEqual(
             html.find("Amazon"), -1,
             "Could not find 'Amazon' in the index page for the AWS public dataset"
+        )
+
+    def test_external_data_s3_with_config(self):
+        skip_without_s3fs()
+        path = test_data.get_external_data("AW3D30_global.vrt")
+        with open(path) as f:
+            xml = f.read()
+        self.assertNotEqual(
+            xml.find("ALPSMLC30_N041W106_DSM"), -1,
+            "Could not find 'ALPSMLC30_N041W106_DSM' in the ALOS VRT")
+
+
+def skip_without_s3fs():
+    try:
+        import s3fs  # noqa
+    except ImportError:
+        raise SkipTest(
+            "Skipping testing external data with s3 because s3fs is not installed"
         )
