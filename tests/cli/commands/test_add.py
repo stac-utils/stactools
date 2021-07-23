268013f4a3264a8604f59d6468414cf2e2b75d43
volaya <volayaf@gmail.com>
2021-07-23 09:08:06 +0200

    added a couple of new tests for 'add' functionality

--- tests/cli/commands/test_add.py
+++ tests/cli/commands/test_add.py
@@ -32,10 +32,59 @@ class AddTest(CliTestCase):
             items = list(target_catalog.get_all_items())
             self.assertEqual(len(items), 5)
 
-            cmd = ['add', item_path, target_catalog.get_self_href()]
+            cmd = ["add", item_path, target_catalog.get_self_href()]
 
             self.run_command(cmd)
 
             target_col = pystac.read_file(target_catalog.get_self_href())
             items = list(target_col.get_all_items())
             self.assertEqual(len(items), 6)
+
+    def test_add_item_to_specific_collection(self):
+        catalog = TestCases.test_case_1()
+        subcatalog = list(list(catalog.get_children())[0].get_children())[0]
+        item = list(subcatalog.get_all_items())[0]
+        item_path = item.get_self_href()
+        with TemporaryDirectory() as tmp_dir:
+            target_catalog = create_temp_catalog_copy(tmp_dir)
+
+            items = list(target_catalog.get_all_items())
+            self.assertEqual(len(items), 5)
+
+            cmd = [
+                "add",
+                item_path,
+                target_catalog.get_self_href(),
+                "--collection",
+                "hurricane-harvey",
+            ]
+
+            res = self.run_command(cmd)
+            self.assertEqual(res.exit_code, 0)
+
+            target_col = pystac.read_file(target_catalog.get_self_href())
+            items = list(target_col.get_all_items())
+            self.assertEqual(len(items), 6)
+
+    def test_add_item_to_missing_collection(self):
+        catalog = TestCases.test_case_1()
+        subcatalog = list(list(catalog.get_children())[0].get_children())[0]
+        item = list(subcatalog.get_all_items())[0]
+        item_path = item.get_self_href()
+        with TemporaryDirectory() as tmp_dir:
+            target_catalog = create_temp_catalog_copy(tmp_dir)
+
+            items = list(target_catalog.get_all_items())
+            self.assertEqual(len(items), 5)
+
+            cmd = [
+                "add",
+                item_path,
+                target_catalog.get_self_href(),
+                "--collection",
+                "WRONG",
+            ]
+
+            res = self.run_command(cmd)
+            self.assertEqual(res.exit_code, 2)
+            self.assertTrue(" A collection with ID WRONG does not exist" in res.output)
