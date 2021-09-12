a05d54f4122206e7b665aeb8da1cd456830964cc
Rob Emanuele <rdemanuele@gmail.com>
2021-09-12 13:33:22 -0400

    Enable external test data from Microsoft Planetary Computer
    This commit allows external data to be fetched from Azure through the
    Microsoft Planetary Compture. Data access requires an anonymous SAS
    token be attached to the URL, which this code fetches and uses for any
    external data marked with "planetary_computer": True.

--- tests/testing/test_test_data.py
+++ tests/testing/test_test_data.py
@@ -21,6 +21,12 @@ class TestDataTest(TestCase):
             xml.find("ALPSMLC30_N041W106_DSM"), -1,
             "Could not find 'ALPSMLC30_N041W106_DSM' in the ALOS VRT")
 
+    def test_external_pc_data(self):
+        path = test_data.get_external_data("manifest.safe")
+        with open(path) as f:
+            xml = f.read()
+        self.assertIsNotNone(xml)
+
 
 def skip_without_s3fs():
     try:
