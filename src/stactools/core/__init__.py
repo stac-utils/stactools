73b93b57df9baa39ff5ba4fd782ab3aab881f4a9
Tyler Battle <tyler@battle.ca>
2021-11-12 14:59:18 -0800

    Switch mypy config to use `strict`, matching pystac. Requires updating `click` to 8.0.x The main difference is that implicit reexports is disabled and untyped decorators are enforced. `click` version 8 adds type annotations, which resolves the untyped decorator issues.

--- src/stactools/core/__init__.py
+++ src/stactools/core/__init__.py
@@ -8,4 +8,16 @@ from stactools.core.merge import (merge_items, merge_all_items)
 from stactools.core.add import add_item
 from stactools.core.addraster import add_raster_to_item
 
+__all__ = [
+    "add_item",
+    "add_raster_to_item",
+    "copy_catalog",
+    "layout_catalog",
+    "merge_all_items",
+    "merge_items",
+    "move_asset_file_to_item",
+    "move_assets",
+    "move_all_assets",
+    "use_fsspec",
+]
 __version__ = "0.2.3"
