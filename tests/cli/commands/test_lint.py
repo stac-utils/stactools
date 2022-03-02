21e103bf5c24d236d6d40d02f2f0aaf2b73bdee8
jonhealy1 <jonathan.d.healy@gmail.com>
2022-03-01 23:36:25 -0800

    feat: add `stac lint` via stac-check
    Adds a CLI command to run linting on a stac object.

---
+++ tests/cli/commands/test_lint.py
@@ -0,0 +1,33 @@
+from typing import List, Callable
+from stactools.cli.commands.lint import create_lint_command
+
+from stactools.testing import CliTestCase
+
+from tests import test_data
+
+
+class LintTest(CliTestCase):
+
+    def create_subcommand_functions(self) -> List[Callable]:
+        return [create_lint_command]
+
+    def test_valid_item(self):
+        path = test_data.get_path(
+            "data-files/linting/20201211_223832_cs2.json"
+        )
+        result = self.run_command(["lint", path])
+        self.assertEqual(0, result.exit_code)
+
+    def test_invalid_item(self):
+        path = test_data.get_path(
+            "data-files/linting/core-item.json"
+        )
+        result = self.run_command(["lint", path])
+        self.assertEqual(1, result.exit_code)
+
+    def test_collection_with_invalid_name(self):
+        path = test_data.get_path(
+            "data-files/catalogs/test-case-1/country-1/area-1-1/collection-invalid.json"
+        )
+        result = self.run_command(["lint", path])
+        self.assertEqual(1, result.exit_code)
