54f18abe2431f39f4291c87e06e8165d88bf08c0
Pete Gadomski <pete.gadomski@gmail.com>
2022-03-03 04:54:08 -0700

    lint: clean up the lint test and add typing

--- tests/cli/commands/test_lint.py
+++ tests/cli/commands/test_lint.py
@@ -1,31 +1,31 @@
 from typing import List, Callable
-from stactools.cli.commands.lint import create_lint_command
+
+from click import Group, Command
 
 from stactools.testing import CliTestCase
+
+from stactools.cli.commands.lint import create_lint_command
 
 from tests import test_data
 
 
 class LintTest(CliTestCase):
 
-    def create_subcommand_functions(self) -> List[Callable]:
+    def create_subcommand_functions(self) -> List[Callable[[Group], Command]]:
         return [create_lint_command]
 
-    def test_valid_item(self):
+    def test_valid_item(self) -> None:
         path = test_data.get_path(
-            "data-files/linting/20201211_223832_cs2.json"
-        )
+            "data-files/linting/20201211_223832_cs2.json")
         result = self.run_command(["lint", path])
         self.assertEqual(0, result.exit_code)
 
-    def test_invalid_item(self):
-        path = test_data.get_path(
-            "data-files/linting/core-item.json"
-        )
+    def test_invalid_item(self) -> None:
+        path = test_data.get_path("data-files/linting/core-item.json")
         result = self.run_command(["lint", path])
         self.assertEqual(1, result.exit_code)
 
-    def test_collection_with_invalid_name(self):
+    def test_collection_with_invalid_name(self) -> None:
         path = test_data.get_path(
             "data-files/catalogs/test-case-1/country-1/area-1-1/collection-invalid.json"
         )
