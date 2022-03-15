e95fe5c774d93a9abf59bbc14ff8f893a48b91ae
Pete Gadomski <pete.gadomski@gmail.com>
2022-03-15 08:52:53 -0600

    feat: add antimeridian helpers to utils
    Includes two helpers:
    - `normalize`: returns a new Polygon that has all of its longitudes in the same
       sign, even if this means going beyond -180 or 180.
    - `split`: returns a new MultiPolygon that contains polygons on either side of
      the antimeridian, per the GeoJSON recommendations.

--- tests/core/test_utils.py
+++ tests/core/test_utils.py
@@ -1,10 +1,16 @@
 from contextlib import contextmanager
+import datetime
 import os.path
 from tempfile import TemporaryDirectory
 import unittest
+from pystac import Item
+import pytest
 
 import rasterio
+import shapely.geometry
+from shapely.geometry import Polygon, MultiPolygon
 
+from stactools.core.utils import antimeridian
 from stactools.core.utils.convert import cogify
 from tests import test_data
 
@@ -32,3 +38,110 @@ class CogifyTest(unittest.TestCase):
             with rasterio.open(outfile) as dataset:
                 self.assertEqual(dataset.compression,
                                  rasterio.enums.Compression.lzw)
+
+    def test_antimeridian_split(self) -> None:
+        # From https://datatracker.ietf.org/doc/html/rfc7946#section-3.1.9
+        canonical = Polygon(
+            ((170, 40), (170, 50), (-170, 50), (-170, 40), (170, 40)))
+        split = antimeridian.split(canonical)
+        assert split
+        expected = MultiPolygon((
+            shapely.geometry.box(170, 40, 180, 50),
+            shapely.geometry.box(-180, 40, -170, 50),
+        ))
+        for actual, expected in zip(split.geoms, expected.geoms):
+            assert actual.equals(expected)
+
+        doesnt_cross = Polygon(
+            ((170, 40), (170, 50), (180, 50), (180, 40), (170, 40)))
+        split = antimeridian.split(doesnt_cross)
+        assert split is None
+
+        canonical_other_way = Polygon(
+            ((-170, 40), (170, 40), (170, 50), (-170, 50), (-170, 40)))
+        split = antimeridian.split(canonical_other_way)
+        assert split
+        expected = MultiPolygon((
+            shapely.geometry.box(-180, 40, -170, 50),
+            shapely.geometry.box(170, 40, 180, 50),
+        ))
+        for actual, expected in zip(split.geoms, expected.geoms):
+            assert actual.equals(
+                expected), f"actual={actual}, expected={expected}"
+
+    def test_antimeridian_split_complicated(self) -> None:
+        complicated = Polygon(((170, 40), (170, 50), (-170, 50), (170, 45),
+                               (-170, 40), (170, 40)))
+        split = antimeridian.split(complicated)
+        assert split
+        expected = MultiPolygon((
+            Polygon(((170, 40), (170, 45), (180, 42.5), (180, 40), (170, 40))),
+            Polygon(((170, 45), (170, 50), (180, 50), (180, 47.5), (170, 45))),
+            Polygon(((-180, 50), (-170, 50), (-180, 47.5), (-180, 50))),
+            Polygon(((-180, 42.5), (-170, 40), (-180, 40), (-180, 42.5))),
+        ))
+        for actual, expected in zip(split.geoms, expected.geoms):
+            assert actual.equals(
+                expected), f"actual={actual}, expected={expected}"
+
+    def test_antimeridian_normalize(self) -> None:
+        canonical = Polygon(
+            ((170, 40), (170, 50), (-170, 50), (-170, 40), (170, 40)))
+        normalized = antimeridian.normalize(canonical)
+        expected = shapely.geometry.box(170, 40, 190, 50)
+        assert normalized.equals(
+            expected), f"actual={normalized}, expected={expected}"
+
+        canonical_other_way = Polygon(
+            ((-170, 40), (170, 40), (170, 50), (-170, 50), (-170, 40)))
+        normalized = antimeridian.normalize(canonical_other_way)
+        expected = shapely.geometry.box(-170, 40, -190, 50)
+        assert normalized.equals(
+            expected), f"actual={normalized}, expected={expected}"
+
+    def test_item_fix_antimeridian_split(self) -> None:
+        canonical = Polygon(
+            ((170, 40), (170, 50), (-170, 50), (-170, 40), (170, 40)))
+        item = Item("an-id",
+                    geometry=shapely.geometry.mapping(canonical),
+                    bbox=canonical.bounds,
+                    datetime=datetime.datetime.now(),
+                    properties={})
+        antimeridian.fix_item(item, antimeridian.Strategy.SPLIT)
+        expected = MultiPolygon((
+            shapely.geometry.box(170, 40, 180, 50),
+            shapely.geometry.box(-180, 40, -170, 50),
+        ))
+        for actual, expected in zip(
+                shapely.geometry.shape(item.geometry).geoms, expected.geoms):
+            assert actual.equals(expected)
+        assert item.bbox == [170, 40, -170, 50]
+
+    def test_item_fix_antimeridian_normalize(self) -> None:
+        canonical = Polygon(
+            ((170, 40), (170, 50), (-170, 50), (-170, 40), (170, 40)))
+        item = Item("an-id",
+                    geometry=shapely.geometry.mapping(canonical),
+                    bbox=canonical.bounds,
+                    datetime=datetime.datetime.now(),
+                    properties={})
+        antimeridian.fix_item(item, antimeridian.Strategy.NORMALIZE)
+        expected = shapely.geometry.box(170, 40, 190, 50)
+        assert shapely.geometry.shape(item.geometry).equals(expected)
+        assert item.bbox
+        assert list(item.bbox) == [170.0, 40.0, 190.0, 50.0]
+
+    def test_item_fix_antimeridian_multipolygon_failure(self) -> None:
+        split = MultiPolygon((
+            shapely.geometry.box(170, 40, 180, 50),
+            shapely.geometry.box(-180, 40, -170, 50),
+        ))
+        item = Item("an-id",
+                    geometry=shapely.geometry.mapping(split),
+                    bbox=split.bounds,
+                    datetime=datetime.datetime.now(),
+                    properties={})
+        with pytest.raises(ValueError):
+            antimeridian.fix_item(item, antimeridian.Strategy.SPLIT)
+        with pytest.raises(ValueError):
+            antimeridian.fix_item(item, antimeridian.Strategy.NORMALIZE)
