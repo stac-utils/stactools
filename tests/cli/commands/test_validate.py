7070cfa8e7861653e6d3111a409386d4ff67961f
Pete Gadomski <pete.gadomski@gmail.com>
2022-03-24 07:00:44 -0600

    fix: typing in validation test file

--- tests/cli/commands/test_validate.py
+++ tests/cli/commands/test_validate.py
@@ -1,48 +1,49 @@
 from typing import Callable, List
 
+from click import Group, Command
 from stactools.cli.commands.validate import create_validate_command
-from stactools.testing import CliTestCase
+from stactools.testing.cli_test import CliTestCase
 from tests import test_data
 
 
 class ValidatateTest(CliTestCase):
-    def create_subcommand_functions(self) -> List[Callable]:
+    def create_subcommand_functions(self) -> List[Callable[[Group], Command]]:
         return [create_validate_command]
 
-    def test_valid_item(self):
+    def test_valid_item(self) -> None:
         path = test_data.get_path(
             "data-files/catalogs/test-case-1/country-1/area-1-1/"
             "area-1-1-imagery/area-1-1-imagery.json"
         )
-        result = self.run_command(["validate", path, "--no-assets"])
+        result = self.run_command(f"validate {path} --no-assets")
         self.assertEqual(0, result.exit_code)
 
-    def test_invalid_item(self):
+    def test_invalid_item(self) -> None:
         path = test_data.get_path(
             "data-files/catalogs/test-case-1/country-1/area-1-1/"
             "area-1-1-imagery/area-1-1-imagery-invalid.json"
         )
-        result = self.run_command(["validate", path])
+        result = self.run_command(f"validate {path}")
         self.assertEqual(1, result.exit_code)
 
-    def test_collection_with_invalid_item(self):
+    def test_collection_with_invalid_item(self) -> None:
         path = test_data.get_path(
             "data-files/catalogs/test-case-1/country-1/area-1-1/collection-invalid.json"
         )
-        result = self.run_command(["validate", path])
+        result = self.run_command(f"validate {path}")
         self.assertEqual(1, result.exit_code)
 
-    def test_collection_with_invalid_item_no_validate_all(self):
+    def test_collection_with_invalid_item_no_validate_all(self) -> None:
         path = test_data.get_path(
             "data-files/catalogs/test-case-1/country-1/area-1-1/collection-invalid.json"
         )
-        result = self.run_command(["validate", path, "--no-recurse"])
+        result = self.run_command(f"validate {path} --no-recurse")
         self.assertEqual(0, result.exit_code)
 
-    def test_collection_invalid_asset(self):
+    def test_collection_invalid_asset(self) -> None:
         path = test_data.get_path(
             "data-files/catalogs/test-case-1/country-1"
             "/area-1-1/area-1-1-imagery/area-1-1-imagery.json"
         )
-        result = self.run_command(["validate", path])
+        result = self.run_command(f"validate {path}")
         self.assertEqual(1, result.exit_code)
