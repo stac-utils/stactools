4cf011cafe22e64f5932f5e94edac0bb47c13eb3
Pete Gadomski <pete.gadomski@gmail.com>
2022-03-24 10:44:11 -0600

    feat: use stac-validator for validation
    Co-authored-by: Phil Varner <pvarner@element84.com>

--- src/stactools/cli/commands/validate.py
+++ src/stactools/cli/commands/validate.py
@@ -1,97 +1,66 @@
+import json
 import sys
-from typing import List, Optional
+from typing import Optional
 
 import click
-import pystac
-from pystac import Catalog, Collection, Item, STACObject, STACValidationError
-
-from stactools.core.utils import href_exists
+from stac_validator.validate import StacValidate
 
 
 def create_validate_command(cli: click.Group) -> click.Command:
     @cli.command("validate", short_help="Validate a stac object.")
     @click.argument("href")
     @click.option(
-        "--recurse/--no-recurse",
+        "--recursive/--no-recursive",
+        help="Recursively validate all STAC objects in this catalog.",
         default=True,
-        help=(
-            "If false, do not validate any children "
-            "(only useful for Catalogs and Collections"
-        ),
     )
     @click.option(
-        "--links/--no-links",
-        default=True,
-        help=("If false, do not check any of the objects's links."),
+        "--validate-links/--no-validate-links", help="Validate links.", default=True
     )
     @click.option(
-        "--assets/--no-assets",
-        default=True,
-        help=("If false, do not check any of the collection's/item's assets."),
+        "--validate-assets/--no-validate-assets", help="Validate assets.", default=True
     )
-    def validate_command(href: str, recurse: bool, links: bool, assets: bool) -> None:
+    @click.option("-v", "--verbose", is_flag=True, help="Enables verbose output.")
+    @click.option(
+        "--quiet/--no-quiet", help="Do not print output to console.", default=False
+    )
+    @click.option(
+        "--log-file",
+        help="Save output to file (local filepath).",
+    )
+    def validate_command(
+        href: str,
+        recursive: bool,
+        validate_links: bool,
+        validate_assets: bool,
+        verbose: bool,
+        quiet: bool,
+        log_file: Optional[str],
+    ) -> None:
         """Validates a STAC object.
 
-        Prints any validation errors to stdout.
+        This is a thin wrapper around
+        [stac-validate](https://github.com/stac-utils/stac-validator). Not all
+        command-line options are exposed. If you want more control over
+        validation, use `stac-validator` directly.
+
+        If you'd like linting, use `stac lint`.
         """
-        object = pystac.read_file(href)
-
-        if isinstance(object, Item):
-            errors = validate(object, None, False, links, assets)
+        validate = StacValidate(
+            href,
+            recursive=recursive,
+            links=validate_links,
+            assets=validate_assets,
+            verbose=verbose,
+            no_output=quiet,
+            log=log_file or "",
+        )
+        is_valid = validate.run()
+        if not quiet:
+            click.echo(json.dumps(validate.message, indent=4))
+        if is_valid:
+            sys.exit(0)
         else:
-            errors = validate(object, object, recurse, links, assets)
-
-        if not errors:
-            click.secho("OK", fg="green", nl=False)
-            click.echo(f" STAC object at {href} is valid!")
-        else:
-            for error in errors:
-                click.secho("ERROR", fg="red", nl=False)
-                click.echo(f" {error}")
             sys.exit(1)
 
     return validate_command
-
-
-def validate(
-    object: STACObject,
-    root: Optional[STACObject],
-    recurse: bool,
-    links: bool,
-    assets: bool,
-) -> List[str]:
-    errors: List[str] = []
-
-    try:
-        object.validate()
-    except FileNotFoundError as e:
-        errors.append(f"File not found: {e}")
-    except STACValidationError as e:
-        errors.append(f"{e}\n{e.source}")
-
-    if links:
-        for link in object.get_links():
-            href = link.get_absolute_href()
-            assert href
-            if not href_exists(href):
-                errors.append(
-                    f'Missing link in {object.self_href}: "{link.rel}" -> {link.href}'
-                )
-
-    if assets and (isinstance(object, Item) or isinstance(object, Collection)):
-        for name, asset in object.get_assets().items():
-            href = asset.get_absolute_href()
-            assert href
-            if not href_exists(href):
-                errors.append(
-                    f"Asset '{name}' does not exist: {asset.get_absolute_href()}"
-                )
-
-    if recurse:
-        assert isinstance(object, Catalog) or isinstance(object, Collection)
-        for child in object.get_children():
-            errors.extend(validate(child, root, recurse, links, assets))
-        for item in object.get_items():
-            errors.extend(validate(item, root, False, links, assets))
-
-    return errors
