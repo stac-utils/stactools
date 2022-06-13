916562a405058cf06b97b13b4fec5064da830599
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2022-06-13 15:46:54 -0500

    Fixed linting errors

--- src/stactools/core/geometry.py
+++ src/stactools/core/geometry.py
@@ -3,17 +3,14 @@ from typing import Any, Dict, List
 import rasterio.features
 
 
-def bounding_box(
-    geom: Dict[str, Any]
-) -> List[float]:
+def bounding_box(geom: Dict[str, Any]) -> List[float]:
     """Extracts and returns the bounding box of a GeoJSON geometry.
 
     Args:
         geom (dict): A STAC Item, FeatureCollection, or GeoJSON geometry
-    
-    Returns:
-        list: A list of float values containing the bounding box of the GeoJSON geometry in the 
-        format [min X, min Y, max X, max Y] 
-    """
 
+    Returns:
+        list: A list of float values containing the bounding box of the GeoJSON geometry in the
+        format [min X, min Y, max X, max Y]
+    """
     return list(rasterio.features.bounds(geom))
