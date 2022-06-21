ad1868894d8ba1ec0fffa5598b262c01548aeccd
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2022-06-21 15:21:04 -0500

    Update geometry.py

--- src/stactools/core/geometry.py
+++ src/stactools/core/geometry.py
@@ -7,7 +7,8 @@ def bounding_box(geom: Dict[str, Any]) -> List[float]:
     """Extracts and returns the bounding box of a GeoJSON geometry.
 
     Args:
-        geom (dict): A GeoJSON Feature, GeoJSON FeatureCollection, GeoJSON geometry, STAC Item, or STAC ItemCollection.
+        geom (dict): A GeoJSON Feature, GeoJSON FeatureCollection, GeoJSON geometry, STAC Item,
+        or STAC ItemCollection.
 
     Returns:
         list: A list of float values containing the bounding box of the GeoJSON geometry in the
