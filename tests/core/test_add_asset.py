0c1084a0c95a9c28139fce6f887ab6e73eab4ee2
Julia Signell <jsignell@gmail.com>
2023-08-04 10:49:03 -0500

    `stac copy --copy-assets` should copy collection assets (#437)
    * Target collection assets as well as item assets
    
    * Update min pystac
    
    * Update changelog

--- tests/core/test_add_asset.py
+++ tests/core/test_add_asset.py
@@ -1,8 +1,10 @@
 import os
+import shutil
+from pathlib import Path
 
 import pystac
 import pytest
-from stactools.core import add_asset_to_item
+from stactools.core import add_asset, add_asset_to_item
 
 from tests import test_data
 
@@ -15,14 +17,14 @@ def item() -> pystac.Item:
 def test_add_asset_to_item(item: pystac.Item) -> None:
     """Test adding an asset to an item without moving the asset"""
     assert "test-asset" not in item.assets
-
     asset = pystac.Asset(
         test_data.get_path("data-files/core/byte.tif"),
         "test",
         "placeholder asset",
         roles=["thumbnail", "overview"],
     )
-    item = add_asset_to_item(item, "test-asset", asset)
+    with pytest.warns(DeprecationWarning, match="Use 'add_asset' instead"):
+        item = add_asset_to_item(item, "test-asset", asset)
 
     asset = item.assets["test-asset"]
     assert isinstance(asset, pystac.Asset), asset
@@ -33,28 +35,38 @@ def test_add_asset_to_item(item: pystac.Item) -> None:
     assert asset.roles == ["thumbnail", "overview"]
 
 
-def test_add_asset_move(tmp_item_path: str, tmp_asset_path: str) -> None:
-    """Test adding an asset to an item and moving it to the item"""
-    item = pystac.Item.from_file(tmp_item_path)
+@pytest.mark.parametrize(
+    "path",
+    [
+        "data-files/core/simple-item.json",
+        "data-files/catalogs/collection-assets/sentinel-2/collection.json",
+    ],
+)
+def test_add_asset_move(path: str, tmp_path: Path, tmp_asset_path: str) -> None:
+    """Test adding and moving an asset to an item or collection"""
+    src = test_data.get_path(path)
+    dst = obj_path = tmp_path / "obj.json"
+    shutil.copyfile(src, str(dst))
+
     asset = pystac.Asset(
         tmp_asset_path,
         "test",
         "placeholder asset",
         roles=["thumbnail", "overview"],
     )
-    item = add_asset_to_item(
-        item, "test-asset", asset, move_assets=True, ignore_conflicts=True
-    )
+    obj = pystac.read_file(obj_path)
 
-    asset = item.assets["test-asset"]
+    obj = add_asset(obj, "test-asset", asset, move_assets=True, ignore_conflicts=True)
+
+    asset = obj.assets["test-asset"]
     assert isinstance(asset, pystac.Asset), asset
     assert asset.href is not None, asset.to_dict()
     assert os.path.isfile(asset.href), asset.to_dict()
     asset_absolute_href = asset.get_absolute_href()
     assert asset_absolute_href
-    item_self_href = item.get_self_href()
-    assert item_self_href
-    assert os.path.dirname(asset_absolute_href) == os.path.dirname(item_self_href)
+    obj_self_href = obj.get_self_href()
+    assert obj_self_href
+    assert os.path.dirname(asset_absolute_href) == os.path.dirname(obj_self_href)
 
 
 def test_add_and_move_with_missing_item_href(item: pystac.Item) -> None:
@@ -69,7 +81,7 @@ def test_add_and_move_with_missing_item_href(item: pystac.Item) -> None:
         roles=["thumbnail", "overview"],
     )
     with pytest.raises(ValueError):
-        add_asset_to_item(item, "test-asset", asset, move_assets=True)
+        add_asset(item, "test-asset", asset, move_assets=True)
 
 
 def test_add_with_missing_item_href_relative_asset_href(item: pystac.Item) -> None:
@@ -84,7 +96,7 @@ def test_add_with_missing_item_href_relative_asset_href(item: pystac.Item) -> No
         roles=["thumbnail", "overview"],
     )
     with pytest.raises(ValueError):
-        add_asset_to_item(item, "test-asset", asset)
+        add_asset(item, "test-asset", asset)
 
 
 def test_add_with_missing_item_href_absolute_asset_href(item: pystac.Item) -> None:
@@ -98,7 +110,7 @@ def test_add_with_missing_item_href_absolute_asset_href(item: pystac.Item) -> No
         "placeholder asset",
         roles=["thumbnail", "overview"],
     )
-    add_asset_to_item(item, "test-asset", asset)
+    add_asset(item, "test-asset", asset)
     asset = item.assets["test-asset"]
     assert isinstance(asset, pystac.Asset), asset
 
@@ -109,7 +121,7 @@ def test_missing_asset_href(item: pystac.Item) -> None:
         "", "test", "placeholder asset", roles=["thumbnail", "overview"]
     )
     with pytest.raises(ValueError):
-        add_asset_to_item(item, "test-asset", asset)
+        add_asset(item, "test-asset", asset)
 
 
 def test_add_asset_ignore_conflict(item: pystac.Item) -> None:
@@ -123,6 +135,6 @@ def test_add_asset_ignore_conflict(item: pystac.Item) -> None:
     )
 
     with pytest.raises(Exception):
-        add_asset_to_item(item, "thumbnail", asset)
-    item = add_asset_to_item(item, "thumbnail", asset, ignore_conflicts=True)
+        add_asset(item, "thumbnail", asset)
+    item = add_asset(item, "thumbnail", asset, ignore_conflicts=True)
     assert item.assets["thumbnail"].title == "Thumbnail"
