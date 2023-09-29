083da25af35927debfe8c514397fe7ffe51dabff
Thomas Maschler <thomas.maschler@planet.com>
2023-09-29 14:06:37 -0400

    make stats and histogram optional #466

--- src/stactools/core/add_raster.py
+++ src/stactools/core/add_raster.py
@@ -18,7 +18,9 @@ logger = logging.getLogger(__name__)
 BINS = 256
 
 
-def add_raster_to_item(item: Item) -> Item:
+def add_raster_to_item(
+    item: Item, statistics: bool = True, histogram: bool = True
+) -> Item:
     """Adds the raster extension to an item.
 
     Args:
@@ -34,27 +36,38 @@ def add_raster_to_item(item: Item) -> Item:
         if asset.roles and "data" in asset.roles:
             raster = RasterExtension.ext(asset)
             href = make_absolute_href(asset.href, item.get_self_href())
-            bands = _read_bands(href)
+            bands = _read_bands(href, statistics, histogram)
             if bands:
                 raster.apply(bands)
     return item
 
 
-def _read_bands(href: str) -> List[RasterBand]:
+def _read_bands(href: str, statistics: bool, histogram: bool) -> List[RasterBand]:
     bands = []
     with rasterio.open(href) as dataset:
         for i, index in enumerate(dataset.indexes):
-            data = dataset.read(index, masked=True)
             band = RasterBand.create()
             band.nodata = dataset.nodatavals[i]
             band.spatial_resolution = dataset.transform[0]
             band.data_type = DataType(dataset.dtypes[i])
-            minimum = float(numpy.min(data))
-            maximum = float(numpy.max(data))
-            band.statistics = Statistics.create(minimum=minimum, maximum=maximum)
-            hist_data, _ = numpy.histogram(data, range=(minimum, maximum), bins=BINS)
-            band.histogram = Histogram.create(
-                BINS, minimum, maximum, hist_data.tolist()
-            )
+
+            if statistics or histogram:
+                data = dataset.read(index, masked=True)
+                minimum = float(numpy.nanmin(data))
+                maximum = float(numpy.nanmax(data))
+            if statistics:
+                band.statistics = Statistics.create(minimum=minimum, maximum=maximum)
+            if histogram:
+                # the entire array is masked, or NAN values are not set to nodata.
+                # won't be able to compute histogram and will return empty array.
+                if numpy.isnan(minimum):
+                    band.histogram = Histogram.create(0, minimum, maximum, [])
+                else:
+                    hist_data, _ = numpy.histogram(
+                        data, range=(minimum, maximum), bins=BINS
+                    )
+                    band.histogram = Histogram.create(
+                        BINS, minimum, maximum, hist_data.tolist()
+                    )
             bands.append(band)
     return bands
