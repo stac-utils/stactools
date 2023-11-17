36ad7cb8faa6cac699a52bb076301327bb89eed8
Pete Gadomski <pete.gadomski@gmail.com>
2023-11-17 07:10:34 -0700

    Make stac-validator and stac-check optional (#468)
    * feat: make stac-validator and stac-check optional
    
    * chore: update changelog

--- src/stactools/cli/__init__.py
+++ src/stactools/cli/__init__.py
@@ -2,6 +2,21 @@
 
 import stactools.core
 
+try:
+    import stac_validator as _
+except ImportError:
+    HAS_STAC_VALIDATOR = False
+else:
+    HAS_STAC_VALIDATOR = True
+
+try:
+    import stac_check as _
+except ImportError:
+    HAS_STAC_CHECK = False
+else:
+    HAS_STAC_CHECK = True
+
+
 stactools.core.use_fsspec()
 
 
@@ -16,13 +31,11 @@ def register_plugin(registry: "Registry") -> None:
         create,
         info,
         layout,
-        lint,
         merge,
         migrate,
         summary,
         update_extent,
         update_geometry,
-        validate,
         version,
     )
 
@@ -35,14 +48,22 @@ def register_plugin(registry: "Registry") -> None:
     registry.register_subcommand(info.create_info_command)
     registry.register_subcommand(info.create_describe_command)
     registry.register_subcommand(layout.create_layout_command)
-    registry.register_subcommand(lint.create_lint_command)
     registry.register_subcommand(merge.create_merge_command)
     registry.register_subcommand(migrate.create_migrate_command)
     registry.register_subcommand(summary.create_summary_command)
-    registry.register_subcommand(validate.create_validate_command)
     registry.register_subcommand(version.create_version_command)
     registry.register_subcommand(update_extent.create_update_extent_command)
     registry.register_subcommand(update_geometry.create_update_geometry_command)
+
+    if HAS_STAC_VALIDATOR:
+        from stactools.cli.commands import validate
+
+        registry.register_subcommand(validate.create_validate_command)
+
+    if HAS_STAC_CHECK:
+        from stactools.cli.commands import lint
+
+        registry.register_subcommand(lint.create_lint_command)
 
 
 from stactools.cli.registry import Registry
